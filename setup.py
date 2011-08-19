5be68cbb35a6a3275b39b2cb9cbc11fe2dbcc1af
Shaun Sephton <shaunsephton@gmail.com>
2011-08-19 10:07:27 +0200

    added testrunner

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test
 
+class TestRunner(test):
+    def run(self, *args, **kwargs):
+        if self.distribution.install_requires:
+            self.distribution.fetch_build_eggs(self.distribution.install_requires)
+        if self.distribution.tests_require:
+            self.distribution.fetch_build_eggs(self.distribution.tests_require)
+        from runtests import runtests
+        runtests()
 setup(
     name='django-export',
     version='0.0.1',
@@ -13,7 +22,12 @@ setup(
         'django-object-tools',
         'django-snippetscream>0.0.4',
     ],
+    tests_require = [
+        'django',
+    ],
     include_package_data=True,
+    test_suite = "export.tests",
+    cmdclass={"test": TestRunner},
     classifiers = [
         "Programming Language :: Python",
         "License :: OSI Approved :: BSD License",
