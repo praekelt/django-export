66460d7156e31e7af15ebb9a35d84590a23da0e5
qoda <jpbydendyk@gmail.com>
2014-09-30 14:53:30 +0200

    Fixed unittests

--- export/tests/__init__.py
+++ export/tests/__init__.py
@@ -7,12 +7,20 @@ class FieldsTestCase(unittest.TestCase):
         from django.db import models
         from export import fields
 
+        # Very likely need to look into adding support for these field types
+        skip_fields = [
+            getattr(models.fields.related, 'ForeignObject', None),
+            getattr(models.fields, 'GenericIPAddressField', None),
+            getattr(models.fields.proxy, 'OrderWrt', None),
+            getattr(models.fields, 'BinaryField', None),
+            getattr(models.fields, 'FilePathField', None)
+        ]
+
         for key, value in models.__dict__.iteritems():
             try:
                 bases = inspect.getmro(value)
             except AttributeError:
-                bases = None
-            if bases:
-                if models.fields.Field in bases and value != \
-                        models.fields.Field:
-                    getattr(fields, key)
+                continue
+
+            if models.fields.Field in bases and value not in skip_fields:
+                assert getattr(fields, key)
