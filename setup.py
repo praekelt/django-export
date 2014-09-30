eae3d11ac30b53190aaf2be91d892dfaa7bd1e58
qoda <jpbydendyk@gmail.com>
2014-09-30 14:52:40 +0200

    Minor refactor

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ setup(
     name='django-export',
     version='0.0.4',
     description='Django app allowing for filtered exporting of model data.',
-    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    long_description=open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
     author_email='dev@praekelt.com',
     url='http://github.com/praekelt/django-export',
-    packages = find_packages(),
-    install_requires = [
-        'django-object-tools',
+    packages=find_packages(),
+    install_requires=[
+        'django-object-tools>=1.0',
         'django-snippetscream>0.0.4',
     ],
-    tests_require = [
-        'django-setuptest>=0.0.6',
+    tests_require=[
+        'django-setuptest>=0.1',
     ],
     include_package_data=True,
-    test_suite = "setuptest.SetupTestSuite",
-    classifiers = [
+    test_suite="setuptest.setuptest.SetupTestSuite",
+    classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: BSD License",
         "Development Status :: 4 - Beta",
