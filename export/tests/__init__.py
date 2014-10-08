5acc1ce282343f8f2dc558c7d8e2ccc65866bfa5
qoda <jpbydendyk@gmail.com>
2014-10-08 09:59:03 +0200

    Added more tests for Export tool class

--- export/tests/__init__.py
+++ export/tests/__init__.py
@@ -1,5 +1,19 @@
+import json
 import inspect
 import unittest
+
+from django.db import models
+
+from export import tools
+
+
+class MockDjangoObject(models.Model):
+    field1 = models.IntegerField()
+    field2 = models.IntegerField()
+
+
+class MockDjangoQuerySet(models.QuerySet):
+    pass
 
 
 class FieldsTestCase(unittest.TestCase):
@@ -24,3 +38,41 @@ class FieldsTestCase(unittest.TestCase):
 
             if models.fields.Field in bases and value not in skip_fields:
                 assert getattr(fields, key)
+
+
+class ToolsTestCase(unittest.TestCase):
+    """
+    Testcase for tools.Export.
+    """
+    def setUp(self):
+        self.export = tools.Export(MockDjangoObject)
+        self.obj = MockDjangoObject(field1=1, field2=2)
+
+    def test_serialize(self):
+        self.assertRaises(TypeError, self.export.serialize, args=[object])
+        self.assertIsInstance(
+            self.export.serialize('json', queryset=[]), unicode
+        )
+        object_list = json.loads(
+            self.export.serialize('json', queryset=[self.obj])
+        )
+        self.assertEqual(object_list[0]['fields']['field1'], self.obj.field1)
+        self.assertEqual(object_list[0]['fields']['field2'], self.obj.field2)
+
+        # ensure fields are honored
+        object_list = json.loads(
+            self.export.serialize(
+                'json', queryset=[self.obj], fields=['field1']
+            )
+        )
+        self.assertEqual(object_list[0]['fields']['field1'], self.obj.field1)
+        self.assertNotIn('field2', object_list[0]['fields'])
+
+    def test_gen_filename(self):
+        self.assertEqual(
+            self.export.gen_filename('json'),
+            "export-export-mockdjangoobject.json"
+        )
+
+    def tearDown(self):
+        pass
