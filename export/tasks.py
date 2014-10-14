2b4de02c1b151576172d7d7355d9fabe2c505b4d
qoda <jpbydendyk@gmail.com>
2014-10-14 14:20:45 +0200

    Added mail task to celery if it exisits

---
+++ export/tasks.py
@@ -0,0 +1,25 @@
+import StringIO
+from zipfile import ZipFile
+
+from django.core.mail import EmailMessage
+
+
+def mail_export(email, filename, data):
+    zip_data = StringIO.StringIO()
+    zipfile = ZipFile(unicode(zip_data), mode='w')
+    zipfile.writestr(filename, data)
+    zipfile.close()
+
+    subject = "Database Export"
+    message = "Database Export Attached"
+    email = EmailMessage(subject, message, to=[email])
+    email.attach(filename, zip_data.getvalue(), 'application/zip')
+    email.send()
+
+    zipfile.close()
+
+try:
+    from celery import task
+    mail_export = task(mail_export, serializer='json')
+except ImportError:
+    pass
