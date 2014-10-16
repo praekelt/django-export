c4b84e6e6256ae3070bd648d0f0dc8db670058cf
qoda <jpbydendyk@gmail.com>
2014-10-16 13:09:42 +0200

    Fixed export mail unicode issues

--- export/utils.py
+++ export/utils.py
@@ -8,13 +8,14 @@ from django.utils.translation import ugettext as _
 def mail_export(email, filename, data):
     zip_data = StringIO.StringIO()
     zipfile = ZipFile(zip_data, mode='w')
-    zipfile.writestr(filename, data)
+    zipfile.writestr(str(filename), str(data))
     zipfile.close()
 
     subject = _("Database Export")
     message = _("Database Export Attached")
     email = EmailMessage(subject, message, to=[email])
     email.attach(filename, zip_data.getvalue(), 'application/zip')
-    email.send()
 
     zip_data.close()
+
+    return email.send()
