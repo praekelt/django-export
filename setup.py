3bbf052d1c20d63f8a8397cbd8902364a8f63388
Phala Ramonyai <phalamag@gmail.com>
2017-05-22 09:15:29 +0200

    Update tox environments

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-export',
-    version='1.9.2',
+    version='1.11.0',
     description='Django app allowing for filtered exporting of model data.',
     long_description=open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Consulting',
