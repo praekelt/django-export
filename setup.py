e8c2bae6dfddce2aea1a190cff5bccef81670492
cblignaut <cilliers@praekeltconsulting.com>
2019-01-04 16:22:26 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -9,23 +9,28 @@ setup(
     author_email='dev@praekelt.com',
     url='http://github.com/praekelt/django-export',
     packages=find_packages(),
-    install_requires=[
-        'django-object-tools',
-        'pyyaml>=3.11'
-    ],
-    tests_require=[
-        'django-setuptest>=0.1',
-    ],
     include_package_data=True,
-    test_suite="setuptest.setuptest.SetupTestSuite",
+    tests_require=[
+        'tox'
+    ],
+    test_suite="tox",
     classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: BSD License",
         "Development Status :: 4 - Beta",
         "Operating System :: OS Independent",
-        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
         "Intended Audience :: Developers",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     zip_safe=False,
 )
