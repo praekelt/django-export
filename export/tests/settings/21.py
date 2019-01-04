10611c96e68bc1c8ce02cc672fbe7607149255f5
cblignaut <cilliers@praekeltconsulting.com>
2019-01-04 16:15:18 +0200

    Added settings and requirement files for tests

---
+++ export/tests/settings/21.py
@@ -0,0 +1,62 @@
+USE_TZ = True
+
+DATABASE_ENGINE = 'sqlite3'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+SECRET_KEY = '123'
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'object_tools',
+    'export'
+]
+
+SERIALIZATION_MODULES = {
+    'csv': 'export.serializers.csv_serializer'
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+
+ROOT_URLCONF = 'object_tools.tests.urls'
+STATIC_URL = '/static/'
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+        ],
+        "OPTIONS": {
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.template.context_processors.request",
+                "django.contrib.messages.context_processors.messages",
+            ],
+            "loaders": [
+                "django.template.loaders.filesystem.Loader",
+                "django.template.loaders.app_directories.Loader",
+            ]
+        },
+    },
+]
+
+MIDDLEWARE = (
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware"
+)
